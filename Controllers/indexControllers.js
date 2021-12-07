dbc1055a0c4da0e5e33fb6ed3f90df0df72b31f9
Jichen Liu <jichen.liu85@gmail.com>
2021-12-07 16:59:35 -0500

    5

--- Controllers/indexControllers.js
+++ Controllers/indexControllers.js
@@ -8,6 +8,12 @@ const homepage_index = (req,res)=>{
    res.render('index',{tonytest:'1234567',blogs:blogArray});	
 }
 
-module.exports={
-	homepage_index
-}
+const bike = {
+  brand: 'feige',
+  model: '28-031'
+};
+
+ module.exports={
+	 homepage_index,
+	 bike
+ }
